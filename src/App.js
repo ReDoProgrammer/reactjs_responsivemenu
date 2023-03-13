a74258044540c41b5ee518a1f918c74a980d4488
redoprogrammer <redoprogrammer@gmail.com>
2023-03-13 09:53:52 +0700

    drawer events when open and close

--- src/App.js
+++ src/App.js
@@ -1,32 +1,50 @@
 import './App.css';
-import { Menu } from 'antd';
+import { Menu, Drawer } from 'antd';
+import { useState } from 'react';
 
 function App() {
+  const [openMenu, setOpenMenu] = useState(false);
   return (
     <div style={{ height: '100vh', backgroundColor: "rgb(0,150,255)" }}>
-      <Menu
-      style={{fontSize:18, fontWeight:"bold"}}
-        mode='horizontal'
-        items={[
-          {
-            label: "Home",
-            key: "home"
-          },
-          {
-            label: "Inventory",
-            key: "inventory"
-          },
-          {
-            label: "Orders",
-            key: "orders"
-          },
-          {
-            label: "Customers",
-            key: "customers"
-          }
-        ]}
-      ></Menu>
+      <Navbar />
+      <Drawer
+        open={openMenu}
+        onClose={() => {
+          setOpenMenu(true);
+        }}
+        closable={false}
+        style={{ border: "none" }}
+      >
+        <Navbar isInline />
+      </Drawer>
     </div>
+  );
+}
+
+function Navbar({ isInline = false }) {
+  return (
+    <Menu
+      style={{ fontSize: 18, fontWeight: "bold" }}
+      mode={isInline ? 'inline' : 'horizontal'}
+      items={[
+        {
+          label: "Home",
+          key: "home"
+        },
+        {
+          label: "Inventory",
+          key: "inventory"
+        },
+        {
+          label: "Orders",
+          key: "orders"
+        },
+        {
+          label: "Customers",
+          key: "customers"
+        }
+      ]}
+    ></Menu>
   );
 }
 
