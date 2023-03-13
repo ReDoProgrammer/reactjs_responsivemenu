96b6d27eb3e966cda84659102c5b57541a43dc56
redoprogrammer <redoprogrammer@gmail.com>
2023-03-13 09:26:33 +0700

    install antd packet and define menu as navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { Menu } from 'antd';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ height: '100vh', backgroundColor: "rgb(0,150,255)" }}>
+      <Menu
+      style={{fontSize:18, fontWeight:"bold"}}
+        mode='horizontal'
+        items={[
+          {
+            label: "Home",
+            key: "home"
+          },
+          {
+            label: "Inventory",
+            key: "inventory"
+          },
+          {
+            label: "Orders",
+            key: "orders"
+          },
+          {
+            label: "Customers",
+            key: "customers"
+          }
+        ]}
+      ></Menu>
     </div>
   );
 }
